041a6504f503e9b5711c6fd81248594eba8a0f67
ShubhamTheSavior <shubham.gadekar99@gmail.com>
2025-07-17 15:28:29 +0530

    code updated

--- src/main/java/com/secureme/SecureMeGatewayApplication.java
+++ src/main/java/com/secureme/SecureMeGatewayApplication.java
@@ -2,12 +2,24 @@ package com.secureme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SecureMeGatewayApplication {
 
+	@Bean
+	public RestTemplateBuilder restTemplateBuilder() {
+		return new RestTemplateBuilder();
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
+		return restTemplateBuilder.setSslBundle(sslBundles.getBundle("secureme-gateway")).build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SecureMeGatewayApplication.class, args);
 	}
